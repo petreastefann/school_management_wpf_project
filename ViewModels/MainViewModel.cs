96044952e5b78e5c7926740d85425f4a873ccdae
petreastefann <petreastefann@gmail.com>
2023-05-14 21:22:40 +0300

    small changes

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,11 +1,26 @@
-﻿using school_management_wpf_project.Views;
+﻿using school_management_wpf_project.Models;
+using school_management_wpf_project.Views;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Controls;
 
 namespace school_management_wpf_project.ViewModels {
 	public class MainViewModel : INotifyPropertyChanged {
+		private User _user;
 		private UserControl _currentView;
+
+		public MainViewModel() {
+			CurrentView = new LoginView();
+			User = new User();
+		}
+
+		public User User {
+			get => _user;
+			set {
+				_user = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(User)));
+			}
+		}
 		public UserControl CurrentView {
 			get => _currentView;
 			set {
@@ -14,9 +29,7 @@ namespace school_management_wpf_project.ViewModels {
 			}
 		}
 
-		public MainViewModel() {
-			CurrentView = new LoginView();
-		}
+		///
 
 		public event PropertyChangedEventHandler PropertyChanged;
 
